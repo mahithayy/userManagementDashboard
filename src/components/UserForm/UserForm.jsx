c134874377c8c3b52d85d8c7d06f3a2b6752ffc7
mahithayy <hollamahitha@gmail.com>
2026-07-03 07:27:44 +0530

    making userForm controlled

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
 import "./UserForm.css";
 
+
 function UserForm({onCancel}) {
+  const [formData, setFormData] = useState({
+  firstName: "",
+  lastName: "",
+  email: "",
+  department: "",
+});
+
+const handleChange = (event) => {
+  const { name, value } = event.target;
+
+  setFormData((prev) => ({
+    ...prev,
+    [name]: value,
+  }));
+};
+
   return (
     <form className="user-form">
       <h2>Add User</h2>
@@ -8,27 +26,45 @@ function UserForm({onCancel}) {
       <input
         type="text"
         placeholder="First Name"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
       />
 
       <input
         type="text"
         placeholder="Last Name"
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
       />
 
       <input
         type="email"
         placeholder="Email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
       />
 
       <input
         type="text"
         placeholder="Department"
+        name="department"
+        value={formData.department}
+        onChange={handleChange}
       />
 
-      <button type="submit">
-        Save User
-      </button>
+      <button
+        type="submit"
+        onClick={(e) => {
+            e.preventDefault();
 
+            console.log(formData);
+          }}
+     >
+        Save User
+     </button>
       <button
         type="button"
         onClick={onCancel}
